6be7bfe6b012a77982b4be51ae2273b0808d0afa
SCARRAIDER <rsahanediriweera@gmail.com>
2022-11-19 22:24:05 +0530

    new actions in intent

--- tryingintent/app/src/main/java/com/example/tryingintent/RecievedMessage.java
+++ tryingintent/app/src/main/java/com/example/tryingintent/RecievedMessage.java
@@ -1,19 +1,14 @@
 package com.example.tryingintent;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.view.View;
-import android.os.Bundle;
-import android.content.Intent;
 
-public class MainActivity extends AppCompatActivity {
+import android.os.Bundle;
+
+public class RecievedMessage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    private void onSendMessage(View view){
+        setContentView(R.layout.activity_recieved_message);
     }
 }
-
