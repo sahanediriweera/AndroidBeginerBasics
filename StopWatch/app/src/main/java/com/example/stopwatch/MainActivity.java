a7833119955bb1eff8693224d97ba439afcfa726
SCARRAIDER <rsahanediriweera@gmail.com>
2023-01-26 10:18:12 +0530

    started the additional functinalities

---
+++ StopWatch/app/src/main/java/com/example/stopwatch/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.stopwatch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int seconds = 0;
+    private boolean running = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickStart(View view){
+        running = true;
+    }
+
+    public void onClickStop(View view){
+        running = false;
+    }
+
+    public void onClickReset(View view){
+        running = false;
+        seconds = 0;
+    }
+
+}
